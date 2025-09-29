a6e04432067db43c7bc2e68f628ac4dd8cd06cbf
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-09-29 10:57:36 +0500

    dashboard

---
+++ admin-dashboard/src/pages/Category/CreateCategory.jsx
@@ -0,0 +1,167 @@
+import { Helmet } from 'react-helmet-async';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {
+    Avatar,
+    Box,
+    Button,
+    Card,
+    Container,
+    FormHelperText,
+    MenuItem,
+    Stack,
+    TextField,
+    Typography,
+    TextareaAutosize,
+    Unstable_Grid2 as Grid,
+    Alert
+} from '@mui/material';
+
+import CheckIcon from '@mui/icons-material/Check';
+
+
+import { useState } from 'react';
+import axios from 'axios';
+import { success } from 'src/theme/colors';
+
+function CreateCategory() {
+
+    const [category, setCategory] = useState({
+        categoryName: "",
+        categoryDescription: ""
+    })
+
+    const [alert, setAlert] = useState({
+        success: true,
+        message: ""
+    })
+
+    const handleInputChange = (e) => {
+        let { name, value } = e.target
+        setCategory((pre) => {
+            return {
+                ...pre,
+                [name]: value
+            }
+        })
+    }
+
+    const handleAddCategory = async () => {
+        // console.log("clickedddd");
+        try {
+            let res = await axios.post("http://localhost:3001/categories", category)
+            console.log(res)
+
+            if (res.data.success) {
+                // alert("Category Added Successfully")
+                setAlert({
+                    success: true,
+                    message: "Category Added Successfully"
+                })
+                setCategory({
+                    categoryName: "",
+                    categoryDescription: ""
+                })
+            }
+            else {
+                // alert(res.data.message)
+                setAlert({
+                    success: false,
+                    message: res.data.message
+                })
+            }
+        }
+        catch (error) {
+            console.log(error)
+            if (error.response) {
+                // alert(error.response.data.message)
+                setAlert({
+                    success: false,
+                    message: error.response.data.message
+                })
+            }
+        }
+    }
+
+    return (
+        <>
+            <Helmet>
+                <title>
+                    Settings | Carpatin Free
+                </title>
+            </Helmet>
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    py: 8
+                }}
+            >
+                <Container maxWidth="xl">
+                    <Stack spacing={3}>
+                        <div>
+                            <Typography variant="h4">
+                                Create Category
+                            </Typography>
+                        </div>
+                        <div>
+                            <Grid
+                                container
+                                spacing={3}
+                            >
+
+                                <Grid
+                                    xs={12}
+                                    md={8}
+                                >
+                                    <Card sx={{ p: 3 }}>
+                                        {
+                                            alert.message && <Alert severity={alert.success ? "success" : "error"}>
+                                                {alert.message}
+                                            </Alert>
+                                        }
+                                        <Box sx={{ maxWidth: 420 }}>
+
+
+
+                                            <Stack spacing={3}>
+                                                <TextField
+                                                    fullWidth
+                                                    label="Category Name"
+                                                    name="categoryName"
+                                                    value={category.categoryName}
+                                                    onChange={handleInputChange}
+
+                                                />
+                                                <TextField
+                                                    fullWidth
+                                                    label="Category Description"
+                                                    name="categoryDescription"
+                                                    value={category.categoryDescription}
+                                                    onChange={handleInputChange}
+
+                                                />
+
+                                            </Stack>
+
+                                            <Box sx={{ mt: 3 }}>
+                                                <Button
+                                                    color="primary"
+                                                    size="large"
+                                                    type="submit"
+                                                    variant="contained"
+                                                    onClick={handleAddCategory}
+                                                >
+                                                    Create                                                </Button>
+                                            </Box>
+                                        </Box>
+                                    </Card>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Stack>
+                </Container>
+            </Box>
+        </>)
+}
+
+export default CreateCategory
---
+++ admin-dashboard/src/routes.jsx
@@ -0,0 +1,53 @@
+import { Outlet } from 'react-router-dom';
+import { Layout as DashboardLayout } from './layouts/dashboard/layout';
+import IconsPage from './pages1/icons';
+import NotFoundPage from './pages1/404';
+import OrdersPage from './pages1/orders';
+import ReportsPage from './pages1';
+import SettingsPage from './pages1/settings';
+import ThemePage from './pages1/theme';
+import CreateCategory from './pages/Category/CreateCategory';
+
+export const routes = [
+  {
+    element: (
+      <DashboardLayout>
+        <Outlet />
+      </DashboardLayout>
+    ),
+    children: [
+      {
+        index: true,
+        element: <ReportsPage />
+      },
+      {
+        path: 'orders',
+        element: <OrdersPage />
+      },
+      {
+        path: 'settings',
+        element: <SettingsPage />
+      },
+      {
+        path: 'theme',
+        element: <ThemePage />
+      },
+      {
+        path: 'icons',
+        element: <IconsPage />
+      },
+      {
+        path: 'create-category',
+        element: <CreateCategory />
+      }
+    ]
+  },
+  {
+    path: '404',
+    element: <NotFoundPage />
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />
+  }
+];
