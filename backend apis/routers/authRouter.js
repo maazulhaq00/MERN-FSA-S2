bb0fce3a8bc48f940b4d4d3c9f0cc78de256d6a6
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-10-22 11:19:30 +0500

    update

--- backend apis/routers/authRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import * as authController from '../controllers/authController.js'
-
-const authRouter = express.Router()
-authRouter.post("/signup", authController.signup)
-authRouter.post("/login", authController.login)
-
-export default authRouter;
